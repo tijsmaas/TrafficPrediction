Fixed adj mx computation

lib/utils.py
@@ -1,5 +1,4 @@
 import scipy.sparse as sp
-import tensorflow as tf
 from lib.dataloaders.dataloader import *
 from scipy.sparse import linalg
 import numpy as np
@@ -72,6 +71,7 @@ def get_total_trainable_parameter_size():
     :return:
     """
     total_parameters = 0
+    import tensorflow as tf
     for variable in tf.trainable_variables():
         # shape is an array of tf.Dimension
         total_parameters += np.product([x.value for x in variable.get_shape()])
@@ -101,6 +101,11 @@ def load_adj(pkl_filename, adjtype):
         adj = [asym_adj(adj_mx)]
     elif adjtype == "doubletransition":
         adj = [asym_adj(adj_mx), asym_adj(np.transpose(adj_mx))]
+    elif adjtype == "random_walk":
+        adj = [np.transpose(calculate_random_walk_matrix(adj_mx))]
+    elif adjtype == "dual_random_walk":
+        adj = [np.transpose(calculate_random_walk_matrix(adj_mx)),
+               np.transpose(calculate_random_walk_matrix(np.transpose(adj_mx)))]
     elif adjtype == "identity":
         adj = [np.diag(np.ones(adj_mx.shape[0])).astype(np.float32)]
     else:

model/pytorch/dcrnn_cell.py
@@ -34,7 +34,7 @@ class LayerParams:
 
 
 class DCGRUCell(torch.nn.Module):
-    def __init__(self, num_units, adj_mx, max_diffusion_step, num_nodes, nonlinearity='tanh',
+    def __init__(self, num_units, supports, max_diffusion_step, num_nodes, nonlinearity='tanh',
                  filter_type="laplacian", use_gc_for_ru=True):
         """
 
@@ -55,16 +55,17 @@ class DCGRUCell(torch.nn.Module):
         self._max_diffusion_step = max_diffusion_step
         self._supports = []
         self._use_gc_for_ru = use_gc_for_ru
-        supports = []
-        if filter_type == "laplacian":
-            supports.append(utils.calculate_scaled_laplacian(adj_mx, lambda_max=None))
-        elif filter_type == "random_walk":
-            supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
-        elif filter_type == "dual_random_walk":
-            supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
-            supports.append(utils.calculate_random_walk_matrix(adj_mx.T).T)
-        else:
-            supports.append(utils.calculate_scaled_laplacian(adj_mx))
+
+        # supports = []
+        # if filter_type == "laplacian":
+        #     supports.append(utils.calculate_scaled_laplacian(adj_mx, lambda_max=None))
+        # elif filter_type == "random_walk":
+        #     supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
+        # elif filter_type == "dual_random_walk":
+        #     supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
+        #     supports.append(utils.calculate_random_walk_matrix(adj_mx.T).T)
+        # else:
+        #     supports.append(utils.calculate_scaled_laplacian(adj_mx))
         for support in supports:
             self._supports.append(self._build_sparse_matrix(support))
 

model/pytorch/dcrnn_model.py
@@ -12,8 +12,7 @@ def count_parameters(model):
 
 
 class Seq2SeqAttrs:
-    def __init__(self, adj_mx, **model_kwargs):
-        self.adj_mx = adj_mx
+    def __init__(self, supports, **model_kwargs):
         self.max_diffusion_step = int(model_kwargs.get('max_diffusion_step', 2))
         self.cl_decay_steps = int(model_kwargs.get('cl_decay_steps', 1000))
         self.filter_type = model_kwargs.get('filter_type', 'laplacian')
@@ -24,13 +23,13 @@ class Seq2SeqAttrs:
 
 
 class EncoderModel(nn.Module, Seq2SeqAttrs):
-    def __init__(self, adj_mx, **model_kwargs):
+    def __init__(self, supports, **model_kwargs):
         nn.Module.__init__(self)
-        Seq2SeqAttrs.__init__(self, adj_mx, **model_kwargs)
+        Seq2SeqAttrs.__init__(self, supports, **model_kwargs)
         self.input_dim = int(model_kwargs.get('input_dim', 1))
         self.seq_len = int(model_kwargs.get('seq_len'))  # for the encoder
         self.dcgru_layers = nn.ModuleList(
-            [DCGRUCell(self.rnn_units, adj_mx, self.max_diffusion_step, self.num_nodes,
+            [DCGRUCell(self.rnn_units, supports, self.max_diffusion_step, self.num_nodes,
                        filter_type=self.filter_type) for _ in range(self.num_rnn_layers)])
 
     def forward(self, inputs, hidden_state=None):
@@ -59,15 +58,15 @@ class EncoderModel(nn.Module, Seq2SeqAttrs):
 
 
 class DecoderModel(nn.Module, Seq2SeqAttrs):
-    def __init__(self, adj_mx, **model_kwargs):
+    def __init__(self, supports, **model_kwargs):
         # super().__init__(is_training, adj_mx, **model_kwargs)
         nn.Module.__init__(self)
-        Seq2SeqAttrs.__init__(self, adj_mx, **model_kwargs)
+        Seq2SeqAttrs.__init__(self, supports, **model_kwargs)
         self.output_dim = int(model_kwargs.get('output_dim', 1))
         self.horizon = int(model_kwargs.get('horizon', 1))  # for the decoder
         self.projection_layer = nn.Linear(self.rnn_units, self.output_dim)
         self.dcgru_layers = nn.ModuleList(
-            [DCGRUCell(self.rnn_units, adj_mx, self.max_diffusion_step, self.num_nodes,
+            [DCGRUCell(self.rnn_units, supports, self.max_diffusion_step, self.num_nodes,
                        filter_type=self.filter_type) for _ in range(self.num_rnn_layers)])
 
     def forward(self, inputs, hidden_state=None):
@@ -95,11 +94,11 @@ class DecoderModel(nn.Module, Seq2SeqAttrs):
 
 
 class DCRNNModel(nn.Module, Seq2SeqAttrs):
-    def __init__(self, adj_mx, logger, **model_kwargs):
+    def __init__(self, supports, logger, **model_kwargs):
         super().__init__()
-        Seq2SeqAttrs.__init__(self, adj_mx, **model_kwargs)
-        self.encoder_model = EncoderModel(adj_mx, **model_kwargs)
-        self.decoder_model = DecoderModel(adj_mx, **model_kwargs)
+        Seq2SeqAttrs.__init__(self, supports, **model_kwargs)
+        self.encoder_model = EncoderModel(supports, **model_kwargs)
+        self.decoder_model = DecoderModel(supports, **model_kwargs)
         self.cl_decay_steps = int(model_kwargs.get('cl_decay_steps', 1000))
         self.use_curriculum_learning = bool(model_kwargs.get('use_curriculum_learning', False))
         self._logger = logger

scripts/gen_adj_mx.py
@@ -8,15 +8,36 @@ import pandas as pd
 import pickle
 
 # Modified to use graph_sensor_locations.csv rather than the sensor_ids file.
+# Modified to use coordinate distances instead of road distances
 
-def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
-    """
+# This only computes approximate distance and does not take into account landscape features (e.g. heights)
+def distanceInKmBetweenEarthCoordinates(c1, c2):
+    lat1, lon1 = c1
+    lat2, lon2 = c2
+    dLat = np.radians(lat2-lat1)
+    dLon = np.radians(lon2-lon1)
+    lat1 = np.radians(lat1)
+    lat2 = np.radians(lat2)
+    a = np.sin(dLat/2) * np.sin(dLat/2) + np.sin(dLon/2) * np.sin(dLon/2) * np.cos(lat1) * np.cos(lat2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    earth_radius_km = 6371
+    return earth_radius_km * c
 
-    :param distance_df: data frame with three columns: [from, to, distance].
-    :param sensor_ids: list of sensor ids.
-    :param normalized_k: entries that become lower than normalized_k after normalization are set to zero for sparsity.
-    :return:
-    """
+def get_sensor_coords(sensor_locations_filename):
+    # Prepare sensor coordinates (distance)
+    sensor_ids_df = pd.read_csv(sensor_locations_filename, dtype={'index': 'int', 'sensor_id': 'str',
+                                                                 'latitude': 'float', 'longitude': 'float'})
+    sensor_coords = [None] * len(sensor_ids_df.values)
+    # Builds sensor id to index map.
+    sensor_id_to_ind = {}
+    for index, sensor_id, lat, long in sensor_ids_df.values:
+        sensor_id_to_ind[sensor_id] = index
+        sensor_coords[index] = np.array([lat, long])
+    return sensor_id_to_ind, np.array(sensor_coords)
+
+def get_road_distances(distances_filename, sensor_ids):
+    # param distance_df: data frame with three columns: [from, to, distance].
+    distance_df = pd.read_csv(distances_filename, dtype={'from': 'str', 'to': 'str'})
     num_sensors = len(sensor_ids)
     dist_mx = np.zeros((num_sensors, num_sensors), dtype=np.float32)
     dist_mx[:] = np.inf
@@ -31,6 +52,16 @@ def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
             continue
         dist_mx[sensor_id_to_ind[row[0]], sensor_id_to_ind[row[1]]] = row[2]
 
+    return sensor_id_to_ind, dist_mx
+
+
+def get_adjacency_matrix(dist_mx, sensor_ids, normalized_k=0.1):
+    """
+    :param sensor_ids: list of sensor ids.
+    :param normalized_k: entries that become lower than normalized_k after normalization are set to zero for sparsity.
+    :return:
+    """
+
     # Calculates the standard deviation as theta.
     distances = dist_mx[~np.isinf(dist_mx)].flatten()
     std = distances.std()
@@ -40,16 +71,16 @@ def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
 
     # Sets entries that lower than a threshold, i.e., k, to zero for sparsity.
     adj_mx[adj_mx < normalized_k] = 0
-    return sensor_ids, sensor_id_to_ind, adj_mx
+    return adj_mx
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sensor_locations_filename', type=str, default='data/sensor_graph/graph_sensor_locations.csv',
                         help='File containing sensor locations, to obtain the right sensor id ordering.')
-    parser.add_argument('--sensor_ids_filename', type=str, default=None,  # default='data/sensor_graph/graph_sensor_ids.txt',
+    parser.add_argument('--sensor_ids_filename', type=str,  # default='data/sensor_graph/graph_sensor_ids.txt',
                         help='File containing sensor ids separated by comma.')
-    parser.add_argument('--distances_filename', type=str, default='data/sensor_graph/distances_la_2012.csv',
+    parser.add_argument('--distances_filename', type=str,  # default='data/sensor_graph/distances_la_2012.csv',
                         help='CSV file containing sensor travel distances with three columns: [from, to, distance].')
     parser.add_argument('--normalized_k', type=float, default=0.1,
                         help='Entries that become lower than normalized_k after normalization are set to zero for sparsity.')
@@ -57,18 +88,25 @@ if __name__ == '__main__':
                         help='Path of the output file.')
     args = parser.parse_args()
 
+    if not args.sensor_ids_filename or not args.distances_filename:
+        sensor_id_to_ind, sensor_coords = get_sensor_coords(args.sensor_locations_filename)
+        dist_km = []
+        for idx, c1 in enumerate(sensor_coords):
+            dist_km.append([distanceInKmBetweenEarthCoordinates(c1, c2) for c2 in sensor_coords])
+        dist_mx = np.array(dist_km)
+
     if args.sensor_ids_filename:
         with open(args.sensor_ids_filename) as f:
             sensor_ids = f.read().strip().split(',')
     else:
-        sensor_ids_df = pd.read_csv(args.sensor_locations_filename, dtype={'index': 'int', 'sensor_id': 'str'})
-        # Convert to list ordered by index
-        sensor_ids = [None] * len(sensor_ids_df.values)
-        for index, sensor_id, *more in sensor_ids_df.values:
-            sensor_ids[index] = sensor_id
+        sensor_ids = list(sensor_id_to_ind.keys())
 
-    distance_df = pd.read_csv(args.distances_filename, dtype={'from': 'str', 'to': 'str'})
-    _, sensor_id_to_ind, adj_mx = get_adjacency_matrix(distance_df, sensor_ids)
+    if args.distances_filename:
+        sensor_id_to_ind, dist_mx = get_road_distances(args.distances_filename, sensor_ids)
+
+    adj_mx = get_adjacency_matrix(dist_mx, sensor_ids)
+    # Fix formatting
+    adj_mx = adj_mx.astype(np.float32)
     # Save to pickle file.
     with open(args.output_pkl_filename, 'wb') as f:
         pickle.dump([sensor_ids, sensor_id_to_ind, adj_mx], f, protocol=2)